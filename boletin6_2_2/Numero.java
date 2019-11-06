14e6842f4630a6edf894b0d756426575cdb511d3
orlando142526 <56253433+orlando142526@users.noreply.github.com>
2019-11-06 11:51:59 +0100

    Update Numero.java

--- boletin6_2_2/Numero.java
+++ boletin6_2_2/Numero.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin6_2_2;
 
-/**
- *
- * @author DANIELCASTELAO\oyagualendara
- */
+
 public class Numero {
+    private short num1;
+    private short num2;
+    private float suma;
+    private float resta;
     
+    public Numero() {
+    }
+
+    public Numero(short num1, short num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public short getNum1() {
+        return num1;
+    }
+
+    public void setNum1(short num1) {
+        this.num1 = num1;
+    }
+
+    public short getNum2() {
+        return num2;
+    }
+
+    public void setNum2(short num2) {
+        this.num2 = num2;
+    }
+    
+    public void operaciones(){
+        if (num1 >= num2) {
+            resta = num1 - num2;
+            System.out.println("El resultado de la resta es "+resta);
+        }
+        suma = num1 + num2;
+System.out.println("El resultado de la suma es "+suma);
 }
