c909687a3d416d082061913ef9b93b785b749eb9
orlando142526 <56253433+orlando142526@users.noreply.github.com>
2019-11-06 11:51:16 +0100

    Update Boletin6_2_2.java

--- boletin6_2_2/Boletin6_2_2.java
+++ boletin6_2_2/Boletin6_2_2.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin6_2_2;
 
-/**
- *
- * @author DANIELCASTELAO\oyagualendara
- */
+import java.util.Scanner;
 public class Boletin6_2_2 {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
+         System.out.println("Escribe un número");
+        Scanner sc = new Scanner(System.in);
+        Numero a = new Numero(sc.nextShort(),sc.nextShort());
+a.operaciones();
+
     }
     
 }
